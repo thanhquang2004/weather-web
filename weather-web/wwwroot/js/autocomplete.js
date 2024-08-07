3a70505a1c2c8ea283c09ce26a31f3ed4ceeac78
NguyenVanQuocLam <qlam162004@gmail.com>
2024-08-07 22:00:06 +0700

    FE search

---
+++ weather-web/wwwroot/js/autocomplete.js
@@ -0,0 +1,38 @@
+let availableKeywords = [
+    'Hà Nội',
+    'Hồ Chí Minh',
+    'Lâm Đồng',
+    'Thừa Thiên Huế',
+    'Đồng Nai',
+    'Bình Định',
+    'Phú Yên',
+];
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result)
+    }
+    display(result);
+
+    if(!result.length){
+        resultsBox.innerHTML = '';
+    }
+}
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick = selectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+function selectInput(list){
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = '';
+}
